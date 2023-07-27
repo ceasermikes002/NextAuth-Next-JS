09f0ebf2066d49b0a5a941a06e11bb99c8399251
chima <chimaemekaiheonu@gmail.com>
2023-07-27 12:47:10 +0100

    Initial Commit

---
+++ ecom-app/app/page.tsx
@@ -0,0 +1,17 @@
+"use client"; 
+import { useSession } from "next-auth/react"; 
+
+export default  function Home() {
+  const {data:session,status}=useSession({
+    required:true,
+  })
+  if(status==="loading"){
+    return <></>
+  }
+  
+  return (
+    <div className='flex h-screen items-center justify-center'>
+      This is a protected page
+    </div>
+  )
+}
---
+++ ecom-app/app/utils/authOptions.tsx
@@ -0,0 +1,28 @@
+import { NextAuthOptions } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    Credentials({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email" },
+        password: { label: "Password" },
+      },
+      authorize: async (credentials, req) => {
+        // Perform Database operations
+
+        if (
+          credentials?.email === "admin@example.com" &&
+          credentials?.password === "admin"
+        ) {
+          return {
+            id: "1",
+            email: "admin@example.com",
+          };
+        }
+        return null;
+      },
+    }),
+  ],
+};
